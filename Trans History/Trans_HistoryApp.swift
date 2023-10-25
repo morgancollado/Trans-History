587cf2b4a322dafdf086224c393e1a814e821a1b
Morgan Collado <morgan.collado@gmail.com>
2023-10-25 13:25:17 -0500

    Initial Commit

---
+++ Trans History/Trans_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trans_HistoryApp.swift
+//  Trans History
+//
+//  Created by Morgan Collado on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Trans_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
